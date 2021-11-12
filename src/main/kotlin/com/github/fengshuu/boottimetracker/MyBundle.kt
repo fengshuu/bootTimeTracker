cbbd04799d2ce9d1eef46dab483d606404b0ed5c
GitHub Action <action@github.com>
2021-11-12 01:55:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/fengshuu/boottimetracker/MyBundle.kt
+++ src/main/kotlin/com/github/fengshuu/boottimetracker/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.fengshuu.boottimetracker
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/fengshuu/boottimetracker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/fengshuu/boottimetracker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.fengshuu.boottimetracker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fengshuu.boottimetracker.MyBundle
 
 class MyApplicationService {
 
